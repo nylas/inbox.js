45b86c0cd8c2bb54b0851e134f8ecfbfdff35219
Caitlin Potter <caitpotter88@gmail.com>
2014-06-04 18:12:15 -0400

    feat(api.js): support fetching Namespace from backend, by namespaceId
    This method should return a promise which resolves with an InboxNamespace object populated with the
    loaded data on success, or is rejected in the case of an aborted or error'd request, with the event
    data for the particular event.

--- src/api/namespace.js
+++ src/api/namespace.js
@@ -1,14 +1,51 @@
+// TODO: support caching.
 InboxAPI.prototype.namespace(namespaceId) {
-  var self = this._;
+  var self = this;
+  var _ = self._;
 
   if (typeof namespaceId !== 'string') {
     throw new TypeError("Unable to perform 'namespace()' on InboxAPI: " +
                         "namespaceId must be a string");
   }
 
-  return self._.promise(function(resolve, reject) {
+  return _.promise(function(resolve, reject) {
     var xhr = XHRForMethod('get');
 
-    
+    AddListeners(xhr, {
+      'load': function(event) {
+        var response = 'response' in xhr ? xhr.response : xhr.responseText;
+        resolve(new InboxNamespace(self, ParseJSON(response)));
+      },
+      // TODO: retry count depending on status?
+      'error': reject,
+
+      'abort': reject
+      // TODO: timeout/progress events are useful.
+    });
+
+    // TODO: headers / withCredentials
+    XHRMaybeJSON(xhr);
+    xhr.open(url, 'get');
+    xhr.send(null);
   });
+}
+
+function InboxNamespace(inbox, data) {
+  if (!(inbox instanceof InboxAPI)) {
+    throw new TypeError("Cannot construct 'InboxNamespace': `inbox` parameter must be InboxAPI");
+  }
+
+  if (!(this instanceof InboxNamespace)) {
+    return new InboxNamespace(inbox, data);    
+  }
+
+  this._ = {
+    inbox: inbox
+  };
+
+  if (data && typeof data === 'object') {
+    Merge(this, data);
+  }
+
+  DefineProperty(this, '_', INVISIBLE);
 }
