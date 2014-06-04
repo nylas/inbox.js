df311a96ea18a9f436c39cced01f112630e9ec64
Caitlin Potter <caitpotter88@gmail.com>
2014-06-04 18:11:22 -0400

    feat(json.js): add wrapper for JSON.parse
    It's a really thin wrapper around JSON.parse, which enables support for legacy browsers and
    polyfills. This may be removed in the future.

---
+++ src/utilities/json.js
@@ -0,0 +1,16 @@
+var ParseJSON = (function() {
+  if (window.JSON && typeof window.JSON.parse === "function") {
+    return function(maybeJSON, replacer, indent) {
+      if (typeof maybeJSON === "string") {
+        return JSON.parse(maybeJSON, replacer, indent);
+      } else {
+        return maybeJSON;
+      }
+    };
+  } else {
+    return function(maybeJSON) {
+      throw new TypeError("Cannot perform 'ParseJSON' on " + maybeJSON + ": JSON.stringify not " +
+                          "available.");
+    }
+  }
+})();
