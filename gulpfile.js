1ce0be8463bb586b1115dec264cc924cb2db3d09
Caitlin Potter <caitpotter88@gmail.com>
2014-06-05 11:12:16 -0400

    chore(gulpfile.js): add jscs task for style linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var jscs = require('gulp-jscs');
 
-gulp.task('default', ['build']);
+gulp.task('default', ['style', 'build']);
+gulp.task('style', ['jscs']);
+
+gulp.task('jscs', function() {
+  return gulp.src([
+    'src/**/*.js',
+    '!src/prefix.js',
+    '!src/suffix.js',
+    'test/**/*.js',
+    '!test/helpers/**/*.js']).
+    pipe(jscs());
+});
 
 gulp.task('build', function() {
   gulp.src(['src/prefix.js', 'src/*/**/*.js', 'src/suffix.js']).
