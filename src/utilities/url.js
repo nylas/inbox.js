d903869f331290da2d6554abceb634818cc99beb
Caitlin Potter <caitpotter88@gmail.com>
2014-06-05 02:10:37 -0400

    feat(utilities): add various helper utilities, fix bug
    New utilities:
    
    - URLAddPaths(url, paths...)   a simple API to help build validURLs
    
    - ParseResponseHeaders(xhr)    a helper for deserializing the result of xhr.getAllResponseHeaders as
                                   JSON
    
    - XHRData(xhr, response)       return useful data from XMLHttpRequest object as JSON, for error
                                   handling
    
    - RejectXHR(reject, xhr, type) Generate a function which will call a rejection handler. This has
                                   been factored out into a helper due to the likelihood that it will be
                                   used frequently.
    
    ---
    
    Minor bugfix:
    
    Caching window.XMLHttpRequest as XHR made mocking difficult, and caused issues for tests. It's much
    simpler to just refer directly to window.XMLHttpRequest.

---
+++ src/utilities/url.js
@@ -0,0 +1,12 @@
+function URLAddPaths(url, paths) {
+  var i, ii;
+  paths = Array.prototype.slice.call(arguments, 1);
+
+  for (i = 0, ii = paths.length; i < ii; ++i) {
+    if (url.charAt(url.length-1) !== '/') {
+      url = url + '/';
+    }
+    url = url + paths[i];
+  }
+  return url;
+}
--- src/utilities/xmlhttprequest.js
+++ src/utilities/xmlhttprequest.js
@@ -1,11 +1,10 @@
 var IE8_METHODS = /^(get|post|head|put|delete|options)$/i;
-var XHR = window.XMLHttpRequest;
 
 function XHRForMethod(method) {
   if (document.documentMode <= 8 && (method.match(IE8_METHODS) || !XHR)) {
     return new window.ActiveXObject("Microsoft.XMLHTTP");
-  } else if (XHR) {
-    return new XHR();
+  } else if (window.XMLHttpRequest) {
+    return new window.XMLHttpRequest();
   }
   return null;
 }
@@ -18,4 +17,44 @@ function XHRMaybeJSON(xhr) {
     // responseType property, which will throw if passed an unsupported
     // DOMString value.
   }
+}
+
+function XHRData(xhr, response) {
+  return {
+    status: xhr.status,
+    statusText: xhr.statusText,
+    data: response,
+    headers: ParseResponseHeaders(xhr)
+  };
+}
+
+function RejectXHR(reject, xhr, type) {
+  return function() {
+    var response = null;
+    if (type === 'json') {
+      response = ParseJSON('response' in xhr ? xhr.response : xhr.responseText);
+    }
+    reject(XHRData(xhr, response));
+  };
+}
+
+function ParseResponseHeaders(xhr) {
+  var headerStr = xhr.getAllResponseHeaders();
+  var headers = {};
+  if (!headerStr) {
+    return headers;
+  }
+  var headerPairs = headerStr.split('\u000d\u000a');
+  for (var i = 0; i < headerPairs.length; i++) {
+    var headerPair = headerPairs[i];
+    // Can't use split() here because it does the wrong thing
+    // if the header value has the string ": " in it.
+    var index = headerPair.indexOf('\u003a\u0020');
+    if (index > 0) {
+      var key = headerPair.substring(0, index);
+      var val = headerPair.substring(index + 2);
+      headers[key] = val;
+    }
+  }
+  return headers;
 }
