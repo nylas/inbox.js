6c6e416813ef142cba353d861133424fca02dd61
Caitlin Potter <caitpotter88@gmail.com>
2014-06-05 12:18:10 -0400

    chore(gulpfile.js): add jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
 
-gulp.task('default', ['style', 'build']);
+var LINT_SRC = [
+  'src/**/*.js',
+  '!src/prefix.js',
+  '!src/suffix.js',
+  'test/**/*.js',
+  '!test/helpers/**/*.js'
+];
+
+gulp.task('default', ['style', 'lint', 'build']);
 gulp.task('style', ['jscs']);
+gulp.task('lint', ['jshint']);
 
 gulp.task('jscs', function() {
-  return gulp.src([
-    'src/**/*.js',
-    '!src/prefix.js',
-    '!src/suffix.js',
-    'test/**/*.js',
-    '!test/helpers/**/*.js']).
+  return gulp.src(LINT_SRC).
     pipe(jscs());
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(LINT_SRC).
+    pipe(jshint()).
+    pipe(jshint.reporter('jshint-stylish')).
+    pipe(jshint.reporter('fail'));
 });
 
 gulp.task('build', function() {
