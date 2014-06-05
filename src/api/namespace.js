9d9630f56efef8b3ec9e66a90e3053ee8880cbf0
Caitlin Potter <caitpotter88@gmail.com>
2014-06-05 02:18:38 -0400

    fix(namespace.js): refactor to reject with data, and throw more meaningful TypeErrors
    Matches the pattern of TypeError throwing from api.js, and provides some useful info when a request
    fails, rather than no info at all.

--- src/api/namespace.js
+++ src/api/namespace.js
@@ -2,30 +2,40 @@
 InboxAPI.prototype.namespace = function(namespaceId) {
   var self = this;
   var _ = self._;
+  var url;
 
-  if (typeof namespaceId !== 'string') {
+  if (namespaceId == null) {
+    throw new TypeError("Unable to perform 'namespace()' on InboxAPI: " +
+                        "missing option `namespaceId`");
+  } else if (typeof namespaceId !== 'string') {
     throw new TypeError("Unable to perform 'namespace()' on InboxAPI: " +
                         "namespaceId must be a string");
   }
+
+  url = URLAddPaths(_.url, namespaceId);
 
   return _.promise(function(resolve, reject) {
     var xhr = XHRForMethod('get');
 
     AddListeners(xhr, {
       'load': function(event) {
-        var response = 'response' in xhr ? xhr.response : xhr.responseText;
-        resolve(new InboxNamespace(self, ParseJSON(response)));
+        var response = ParseJSON('response' in xhr ? xhr.response : xhr.responseText);
+        if (xhr.status >= 200 && xhr.status < 300) {
+          resolve(new InboxNamespace(self, response));
+        } else {
+          reject(XHRData(xhr, response));
+        }
       },
       // TODO: retry count depending on status?
-      'error': reject,
+      'error': RejectXHR(reject, xhr, 'json'),
 
-      'abort': reject
+      'abort': RejectXHR(reject, xhr, 'json')
       // TODO: timeout/progress events are useful.
     });
 
     // TODO: headers / withCredentials
     XHRMaybeJSON(xhr);
-    xhr.open(url, 'get');
+    xhr.open('get', url);
     xhr.send(null);
   });
 }
@@ -40,7 +50,8 @@ function InboxNamespace(inbox, data) {
   }
 
   this._ = {
-    inbox: inbox
+    inbox: inbox,
+    url: URLAddPaths(inbox._.url, data.id || data.namespace)
   };
 
   if (data && typeof data === 'object') {
