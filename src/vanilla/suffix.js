2258ed9552356581d67ffe987a0edb37609d9879
Caitlin Potter <caitpotter88@gmail.com>
2014-06-24 13:39:35 -0400

    refactor(inbox.js): add RequireJS and Browserify exports to vanilla inbox.js

--- src/vanilla/suffix.js
+++ src/vanilla/suffix.js
@@ -1,4 +1,13 @@
-// TODO: AMD/CJS exports here
-window.InboxAPI = InboxAPI;
+if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+  // RequireJS
+  define(function(angular) {
+    return InboxAPI;
+  });
+} else if (typeof module === 'object' && typeof require === 'function') {
+  // CommonJS/Browserify
+  module.exports = InboxAPI;
+} else {
+  window.InboxAPI = InboxAPI;
+}
 
 })(this);
