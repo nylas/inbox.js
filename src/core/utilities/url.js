8c968751a6da129ec33a1a77ae4f7e9813ab3083
Caitlin Potter <caitpotter88@gmail.com>
2014-06-25 23:50:11 -0400

    feat(filters): add InboxURLFilters to convert filter objects to query strings
    The method is fairly complex, and will hopefully be simplified over time.

--- src/core/utilities/url.js
+++ src/core/utilities/url.js
@@ -27,3 +27,158 @@ function URLFormat(template, args) {
     return '';
   });
 }
+
+var ARRAY_BRACKET_REGEXP = /\[\]$/;
+
+function BuildURLParams(key, value, add) {
+  var i, ii, name, v, classicArray = false;
+  if (IsArray(value)) {
+    classicArray = key.test(ARRAY_BRACKET_REGEXP);
+    for (i=0, ii=value.length; i<ii; ++i) {
+      v = value[i];
+      if (classicArray) {
+        add(key, v);
+      } else {
+        BuildURLParams(key + '[' + (typeof v === 'object' ? i : '') + ']', v, add);
+      }
+    }
+  } else if (typeof value === 'object') {
+    for (name in value) {
+      BuildURLParams(key + '[' + name + ']', value[name], add);
+    }
+  } else {
+    add(key, value);
+  }
+}
+
+// Based on jQuery.param (2.x.x)
+function SerializeURLParams(params) {
+  var key, s;
+  function add(key, value) {
+    value = typeof value === 'function' ? value() : value == null ? '' : value;
+    s[s.length] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+  }
+
+  if (typeof params === 'object') {
+    s = [];
+    for (key in params) {
+      if (params.hasOwnProperty(key)) {
+        BuildURLParams(key, params[key], add);
+      }
+    }
+  }
+
+  return s.join('&').replace(/%20/, '+');
+}
+
+// supported filter options. if their value is 'true', their new key is the same as their old key.
+// if their value is a string, it is the name of their valid key.
+var FILTER_NAMES_OPTS = {
+  'subject': true,
+  'email': 'any_email',
+  'from': true,
+  'to': true,
+  'cc': true,
+  'bcc': true,
+  'thread': true,
+  'tag': true,
+  'filename': true,
+  'lastMessageBefore': 'last_message_before',
+  'lastMessageAfter': 'last_message_after',
+  'startedBefore': 'started_before',
+  'startedAfter': 'started_after',
+  'limit': true,
+  'offset': true
+};
+
+// After converting to filtered names, these parameters must be converted to unix timestamps.
+var FILTER_DATES = {
+  'last_message_before': true,
+  'last_message_after': true,
+  'started_before': true,
+  'started_after': true
+};
+
+var FILTER_REGEXPS = {
+  'subject': true
+};
+
+var FILTER_STRINGS = {
+  'subject': true,
+  'any_email': true,
+  'from': true,
+  'to': true,
+  'cc': true,
+  'bcc': true,
+  'thread': true,
+  'tag': true,
+  'filename': true
+};
+
+var FILTER_INTS = {
+  'limit': true,
+  'offset': true
+};
+
+var INT_REGEXP = /^((0x[0-9a-f]+)|([0-9]+))$/i;
+
+function InboxURLFilters(filters) {
+  var params;
+  var key;
+  var value;
+  var result = '';
+  if (!filters || typeof filters !== "object") {
+    return '';
+  }
+
+  params = {};
+
+  for (key in filters) {
+    value = FILTER_NAMES_OPTS[key];
+    if (value === true) {
+      params[key] = filters[key];
+    } else if (typeof value === 'string') {
+      params[value] = filters[key];
+    }
+  }
+
+  for (key in params) {
+    value = params[key];
+    if (typeof value === 'function') {
+      value = value();
+    }
+
+    if (FILTER_DATES[key] === true) {
+      if (typeof value === 'number' || typeof value === 'string' || typeof value === 'object') {
+        value = ToUnixTimestamp(value);
+        params[key] = value;
+        if (typeof value === 'number' && ((value !== value) || (Math.abs(value) === Infinity))) {
+          // NaN/Infinity timestamp --- don't send.
+          delete params[key];
+        }
+      } else {
+        // Invalid timestamp
+        delete params[key];
+      }
+    } else if (FILTER_REGEXPS[key] === true && params[key] instanceof RegExp) {
+      params[key] = params[key].toString();
+    } else if (FILTER_INTS[key] === true) {
+      if ((typeof value === 'string' && INT_REGEXP.test(value)) || typeof value === 'number') {
+        value = Number(value);
+        if (value === value && Math.abs(value) !== Infinity) {
+          params[key] = value;
+        } else {
+          delete params[key];
+        }
+      } else {
+        delete params[key];
+      }
+    }
+    if (FILTER_STRINGS[key] === true && typeof params[key] === 'object') {
+      delete params[key];
+    }
+  }
+
+  result = SerializeURLParams(params);
+  return result ? '?' + result : '';
+}
--- test/utilities/filters.spec.js
+++ test/utilities/filters.spec.js
@@ -1,9 +1,297 @@
-ddescribe('URLAddFilters', function() {
-  it('should not remove query parameters already in url', function() {
-    var anchor = document.createElement('a');
-    anchor.href = URLAddFilters('http://api.inboxapp.co/n/fake_namespace_id/threads?foo=bar', {
-      subject: 'Hello!'
+describe('InboxURLFilters', function() {
+  var i, ii, spec;
+  var regexpFilters = ['subject'];
+
+  function parseQuery(str) {
+    var query = {};
+    var strs, i, ii, tuple;
+    if (str && str.charAt(0) === '?') {
+      str = str.slice(1);
+      strs = str.split('&');
+      for (i=0, ii=strs.length; i<ii; ++i) {
+        tuple = strs[i].split('=');
+        if (tuple && tuple.length === 2) {
+          query[decodeURIComponent(tuple[0])] = decodeURIComponent(tuple[1]);
+        }
+      }
+    }
+    return query;
+  }
+
+  beforeEach(function() {
+    spec = this;
+    this.addMatchers({
+      toBeQuery: function(expected) {
+        expected = expected.replace(/\%\@/g, spec.replaced);
+        return jasmine.Matchers.prototype.toBe.call(this, expected);
+      }
     });
-    console.log(anchor.href);
+  });
+
+  function setName(name) {
+    return function() {
+      this.replaced = null;
+      this.replaced = FILTER_NAMES_OPTS[name];
+      if (this.replaced === true) {
+        this.replaced = name;
+      }
+    };
+  }
+
+
+  for (i=0, ii=regexpFilters.length; i<ii; ++i) {
+    (function(name) {
+      describe(name, function() {
+        beforeEach(setName(name));
+
+        function re(val) {
+          var obj = {};
+          obj[name] = val;
+          return InboxURLFilters(obj);
+        }
+
+
+        it('should accept RegExp', function() {
+          expect(re(/^test message$/)).toBeQuery('?%@=%2F%5Etest+message%24%2F');
+        });
+
+
+        it('should accept String', function() {
+          expect(re('test message')).toBeQuery('?%@=test+message');
+        });
+
+
+        it('should not serialize non-RegExp objects', function() {
+          expect(re({foo: 'bar'})).toBeQuery('');
+        });
+
+
+        it('should serialize result of functions', function() {
+          expect(re(function() { return 'hello'; })).toBeQuery('?%@=hello');
+        });
+      });
+    })(regexpFilters[i]);
+  }
+
+
+  var stringFilters = ['email', 'from', 'to', 'cc', 'bcc', 'thread', 'tag', 'filename'];
+  for (i=0, ii=stringFilters.length; i<ii; ++i) {
+    (function(name) {
+      describe(name, function() {
+        beforeEach(setName(name));
+
+        function str(txt) {
+          var obj = {};
+          obj[name] = txt;
+          return InboxURLFilters(obj);
+        }
+
+
+        it('should accept String', function() {
+          expect(str('bitdiddler@inboxapp.com')).toBeQuery('?%@=bitdiddler%40inboxapp.com');
+        });
+
+
+        it('should accept Number', function() {
+          expect(str(1)).toBeQuery('?%@=1');
+          expect(str(Infinity)).toBeQuery('?%@=Infinity');
+          expect(str(NaN)).toBeQuery('?%@=NaN');
+        });
+
+
+        it('should accept Boolean', function() {
+          expect(str(true)).toBeQuery('?%@=true');
+          expect(str(false)).toBeQuery('?%@=false');
+        });
+
+
+        it('should not serialize RegExp', function() {
+          expect(str(/bitdiddler@inboxapp.com/)).toBeQuery('');
+        });
+
+
+        it('should not serialize objects', function() {
+          expect(str({foo: 'bar'})).toBeQuery('');
+        });
+
+
+        it('should serialize result of functions', function() {
+          expect(str(function() { return 'hello'; })).toBeQuery('?%@=hello');
+        });
+      });
+    })(stringFilters[i]);
+  }
+
+
+  var timestampFilters = [
+    'lastMessageBefore',
+    'lastMessageAfter',
+    'startedBefore',
+    'startedAfter'
+  ];
+  for (i=0, ii=timestampFilters.length; i<ii; ++i) {
+    (function(name) {
+      describe(name, function() {
+        beforeEach(setName(name));
+
+        function ts(txt) {
+          var obj = {};
+          obj[name] = txt;
+          return InboxURLFilters(obj);
+        }
+
+
+        it('should accept numeric String', function() {
+          expect(ts('12345678')).toBeQuery('?%@=12345678');
+        });
+
+
+        it('should accept ISO-8601 String', function() {
+          expect(ts('2014-06-19')).toBeQuery('?%@=1403136000000');
+          expect(ts('2014-06-19T19:12:32+00:00')).toBeQuery('?%@=1403205152000');
+          expect(ts('2014-06-19T19:12:32Z')).toBeQuery('?%@=1403205152000');
+        });
+
+
+        it('should accept Number', function() {
+          expect(ts(1403205152000)).toBeQuery('?%@=1403205152000');
+        });
+
+
+        it('should NOT accept number NaN', function() {
+          expect(ts(NaN)).toBe('');
+          expect(ts(-NaN)).toBe('');
+        });
+
+
+        it('shoud NOT accept number Infinity', function() {
+          expect(ts(Infinity)).toBe('');
+          expect(ts(-Infinity)).toBe('');
+        });
+
+
+        it('should NOT accept Boolean', function() {
+          expect(ts(true)).toBe('');
+          expect(ts(false)).toBe('');
+        });
+
+
+        it('should NOT serialize RegExp', function() {
+          expect(ts(/Monday, march 7th/)).toBeQuery('');
+        });
+
+
+        it('should NOT serialize objects', function() {
+          expect(ts({date: 'Monday, march 7th'})).toBeQuery('');
+        });
+
+
+        it('should serialize Date', function() {
+          var date = new Date(1403205152000);
+          expect(ts(date)).toBeQuery('?%@=1403205152000');
+        });
+
+
+        it('should serialize result of functions', function() {
+          expect(ts(function() { return '1403205152000'; })).toBeQuery('?%@=1403205152000');
+        });
+      });
+    })(timestampFilters[i]);
+  }
+
+
+  var integerFilters = ['limit', 'offset'];
+  for (i=0, ii=integerFilters.length; i<ii; ++i) {
+    (function(name) {
+      describe(name, function() {
+        beforeEach(setName(name));
+
+        function int(txt) {
+          var obj = {};
+          obj[name] = txt;
+          return InboxURLFilters(obj);
+        }
+
+
+        it('should accept numeric String', function() {
+          expect(int('12345678')).toBeQuery('?%@=12345678');
+          expect(int('0xFFFF')).toBeQuery('?%@=65535');
+        });
+
+
+        it('should NOT accept non-integer String', function() {
+          expect(int('2014-06-19')).toBeQuery('');
+          expect(int('12345678.44')).toBeQuery('');
+
+        });
+
+
+        it('should accept Number', function() {
+          expect(int(1403205152000)).toBeQuery('?%@=1403205152000');
+        });
+
+
+        it('should NOT accept number NaN', function() {
+          expect(int(NaN)).toBe('');
+          expect(int(-NaN)).toBe('');
+        });
+
+
+        it('shoud NOT accept number Infinity', function() {
+          expect(int(Infinity)).toBe('');
+          expect(int(-Infinity)).toBe('');
+        });
+
+
+        it('should NOT accept Boolean', function() {
+          expect(int(true)).toBe('');
+          expect(int(false)).toBe('');
+        });
+
+
+        it('should NOT serialize RegExp', function() {
+          expect(int(/Monday, march 7th/)).toBeQuery('');
+        });
+
+
+        it('should NOT serialize objects', function() {
+          expect(int({date: 'Monday, march 7th'})).toBeQuery('');
+        });
+
+
+        it('should not serialize Date', function() {
+          var date = new Date(1403205152000);
+          expect(int(date)).toBeQuery('');
+        });
+
+
+        it('should serialize result of functions', function() {
+          expect(int(function() { return '1403205152000'; })).toBeQuery('?%@=1403205152000');
+        });
+      });
+    })(integerFilters[i]);
+  }
+
+
+  it('should remove unknown filters', function() {
+    expect(InboxURLFilters({
+      'NOT_A_REAL_FILTER': '1403205152000'
+    })).toBe('');
+  });
+
+
+  it('should combine filters', function() {
+    var date = new Date();
+    expect(parseQuery(InboxURLFilters({
+      subject: /^foo bar$/,
+      lastMessageBefore: date,
+      email: 'natasha@evilspy.com',
+      from: 'boris@evilspy.com'
+    }))).toContainObject({
+      subject: '/^foo+bar$/',
+      last_message_before: '' + date.getTime(),
+      any_email: 'natasha@evilspy.com',
+      from: 'boris@evilspy.com'
+    });
   });
 });
