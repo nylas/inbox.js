5735ad1e91469e564c1118ce5815e39d0c507742
Caitlin Potter <caitpotter88@gmail.com>
2014-07-05 20:59:09 -0400

    docs(core): Add JSDOC comments for core classes and methods.
    Not perfect, and likely contains some mistakes, however it's likely a good first attempt at documenting
    the JS SDK.

--- src/core/api/messages.js
+++ src/core/api/messages.js
@@ -1,3 +1,19 @@
+/**
+ * @class InboxMessage
+ *
+ * Class which represents a specific Message associated with a Thread.
+ *
+ * @param {InboxThread} thread The InboxThread object which this InboxMessage is to be
+ *   associated with.
+ *
+ * @param {Object} data The data associated with a given InboxThread. This data arrives from a
+ *   successful response from a server, and should always contain the expected properties. However,
+ *   it is necessary for the object to contain a property `id`.
+ *
+ * @throws {TypeError} The InboxThread constructor will throw under the circumstances that we have
+ *   no `id` property in the response, or if the response object is null or undefined,
+ *   or if the `thread` parameter is not an instance of InboxThread.
+ */
 function InboxMessage(thread, data) {
   var namespace;
   if (!(thread instanceof InboxThread)) {
