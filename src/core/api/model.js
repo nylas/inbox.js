nylas/inbox.js

commit e0e0151db68dd10cea1431f8742bb89d2c2fe6f5

fix(INNamespace): respect resourceMapping when updating threads and tags
Previously, when merging properties into objects being updated, the properties were not transformed
into client-side properties (defined in the resourceMapping for the model). This CL provides simple
machinery for ensuring that this works as expected.

In addition, machinery is added for merging simple arrays, such as messageIDs or draftIDs.

Still TODO: support correctly merging participants and tag data arrays during update.

Closes #30
Closes #32

src/core/api/model.js
@@ -253,6 +253,7 @@ INModelObject.prototype.toJSON = function() {
   return toJSON(this.raw());
 };
 
+
 var casters = {
   array: {
     to: function castToArray(val) {
@@ -261,6 +262,18 @@ var casters = {
     },
     from: function castFromArray(val) {
       return val;
+    },
+    merge: function mergeSimpleArrays(dest, src) {
+      // Merge simple arrays
+      if (!isArray(dest)) return merge([], src || []);
+      else if (!isArray(src) || !src.length) dest.length = 0;
+      else {
+        dest.length = src.length;
+        for (var i=0, ii = src.length; i < ii; ++i) {
+          dest[i] = src[i];
+        }
+      }
+      return dest;
     }
   },
   date: {
@@ -430,6 +443,66 @@ function defineResourceMapping(resourceClass, mapping, base) {
   defineProperty(resourceClass, 'resourceMapping', INVISIBLE, null, null, resourceMapping);
   defineProperty(resourceClass.prototype, 'resourceMapping', INVISIBLE, null, null,
     resourceMapping);
+}
+
+
+function mappingForProperty(propertyName, resource) {
+  if (propertyName === 'jsonKeys' || propertyName === 'resourceMapping') return;
+  var mapping = resource && resource.resourceMapping;
+  if (mapping) {
+    if (mapping.hasOwnProperty(propertyName)) {
+      return mapping[propertyName];
+    } else if (mapping.jsonKeys.hasOwnProperty(propertyName)) {
+      return mapping[mapping.jsonKeys[propertyName]];
+    }
+  }
+}
+
+
+/**
+ * @function
+ * @name convertFromRaw
+ * @private
+ *
+ * @description
+ * Convert a raw JSON object into an object similar to what the resource would look like, by
+ * transforming the properties in the same way.
+ *
+ * @param {object} object raw object from the server
+ * @param {INModelObject} resource resource class
+ */
+function convertFromRaw(object, resource) {
+  var mapping = resource.resourceMapping;
+  var out;
+  if (!mapping) return;
+  out = {};
+
+  forEach(mapping.jsonKeys, function copyMappedProperties(propertyName, jsonKey) {
+    var mappingInfo = mapping[propertyName];
+    var cast = mappingInfo.to;
+    var cnst = mappingInfo.cnst;
+    var isObject;
+    var currentValue;
+
+    if (hasProperty(object, jsonKey)) {
+      currentValue = object[jsonKey];
+      if (propertyName !== jsonKey) {
+        delete object[jsonKey];
+      }
+
+      if (cnst) {
+        object[propertyName] = cnst;
+      } else {
+        cast = cast(currentValue, mappingInfo);
+        isObject = cast && typeof cast === 'object';
+        if (typeof currentValue !== 'undefined') {
+          object[propertyName] = cast;
+        }
+      }
+    } else if (cnst) {
+      object[propertyName] = cnst;
+    }
+  });
 }
 
 

test/api/threads.spec.js
@@ -274,6 +274,18 @@ describe('INThread', function() {
       mockPromises.executeForPromise(promise);
       expect(fulfilled).toHaveBeenCalled();
     });
+
+
+    iit('should update threads consistent with resourceMapping (issue #30)', function() {
+      var oldThreads = [new INThread(inbox, mockThread1)];
+      var fulfilled = jasmine.createSpy('load').andCallFake(function(threads) {
+        expect(threads[0]).toContainObject(mappedThread1Updated2);
+      });
+      var promise = namespace.threads(oldThreads).then(fulfilled);
+      server.respond([200, { "Content-Type": "application/json" }, JSON.stringify([mockThread1Updated2])]);
+      mockPromises.executeForPromise(promise);
+      expect(fulfilled).toHaveBeenCalled();
+    });
   });
 
 
@@ -362,6 +374,6 @@ describe('INThread', function() {
       server.respond([200, { "Content-Type": "application/json" }, JSON.stringify(mockThread1Updated2)]);
       mockPromises.executeForPromise(promise);
       expect(fulfilled).toHaveBeenCalled();
-    })
+    });
   });
 });

.jshintrc
@@ -26,6 +26,7 @@
     "apiRequestPromise": false,
     "applyFilters": false,
     "capitalizeString": false,
+    "convertFromRaw": false,
     "defineProperty": false,
     "defineResourceMapping": false,
     "deleteModel": false,
@@ -41,6 +42,7 @@
     "isBlob": false,
     "isFile": false,
     "map": false,
+    "mappingForProperty": false,
     "merge": false,
     "mergeArray": false,
     "noop": false,

src/core/api/models/namespace.js
@@ -217,7 +217,7 @@ INNamespace.prototype.threads = function(optionalThreadsOrFilters, filters) {
         return resolve(mergeArray(updateThreads, set, 'id', function(data) {
           cache.persist(data.id, data, noop);
           return new INThread(self, data);
-        }));
+        }, INThread));
       }
 
       resolve(map(set, function(item) {
@@ -283,7 +283,7 @@ INNamespace.prototype.tags = function(optionalTagsOrFilters, filters) {
         return resolve(mergeArray(updateTags, set, 'id', function(data) {
           cache.persist(data.id, data, noop);
           return new INTag(self, data);
-        }));
+        }, INTag));
       }
 
       resolve(map(set, function(item) {

src/core/utilities/data.js
@@ -39,26 +39,38 @@ function noop() {}
  *
  * @param {object|Array} dest the destination object to which items are copied.
  * @param {object|Array} src the source object from which items are copied.
+ * @param {INModelObject=} resource optional resource class which defines how to merge properties.
  *
  * @returns {object|Array} the destination object.
  */
-function merge(dest, src) {
+function merge(dest, src, resource) {
   var key;
   var a;
   var b;
+  var mapping;
   for (key in src) {
-    if (key !== '_' && src.hasOwnProperty(key)) {
-      b = src[key];
-      if (dest.hasOwnProperty(key)) {
-        a = dest[key];
-        if (typeof a === 'object' && typeof b === 'object') {
-          var start = a ? (isArray(a) ? [] : {}) : null;
-          dest[key] = merge(merge(start, a), b);
-          continue;
+    if (key !== '_') {
+      mapping = resource && mappingForProperty(key, resource);
+      if (src.hasOwnProperty(key)) {
+        b = src[key];
+        if (dest.hasOwnProperty(key)) {
+          a = dest[key];
+          if (typeof a === 'object' && typeof b === 'object') {
+            if (mapping) {
+              if (mapping.merge) dest[key] = mapping.merge(a, b);
+            } else {
+              var start = a ? (isArray(a) ? [] : {}) : null;
+              dest[key] = merge(merge(start, a), b, true);
+            }
+            continue;
+          }
+          dest[key] = b;
+        } else {
+          dest[key] = b;
         }
-        dest[key] = b;
-      } else {
-        dest[key] = b;
+      } else if (mapping) {
+        // If there's a mapping for this property, delete it.
+        delete dest[key];
       }
     }
   }
@@ -82,10 +94,13 @@ function merge(dest, src) {
  * @param {string} id the property name by which objects are identified. This is typically 'id'.
  * @param {function=} newCallback a callback which modifies a new value in some way, typically by
  *   constructing a new INModelObject with it.
+ * @param {INModelObject=} resource the constructor for the resource type of which the arrays
+ *   should hold. Optional, but useful for ensuring properties are copied with regard for the
+ *   resource mapping.
  *
  * @returns {Array} the oldArray.
  */
-function mergeArray(oldArray, newArray, id, newCallback) {
+function mergeArray(oldArray, newArray, id, newCallback, resource) {
   var objects = {};
   var seen = {};
   var numOld = 0;
@@ -95,6 +110,7 @@ function mergeArray(oldArray, newArray, id, newCallback) {
   var callback = typeof newCallback === 'function' && newCallback;
   var i, ii, obj, oldObj;
   var oldLength = oldArray.length;
+
   for (i=0, ii=oldLength; i<ii; ++i) {
     obj = oldArray[i];
     if (typeof obj === 'object' && id in obj) {
@@ -102,13 +118,19 @@ function mergeArray(oldArray, newArray, id, newCallback) {
       objects[obj[id]] = obj;
     }
   }
+
   for (i=0, ii=newArray.length; i<ii; ++i) {
     obj = newArray[i];
+
+    if (resource && resource.resourceMapping) {
+      convertFromRaw(obj, resource);
+    }
+
     if (typeof obj === 'object' && id in obj) {
       ++numNew;
       if ((oldObj = objects[obj[id]])) {
         ++numSeen;
-        merge(oldObj, obj);
+        merge(oldObj, obj, resource);
         seen[obj[id]] = true;
       } else {
         if (callback) obj = callback(obj);
@@ -118,6 +140,7 @@ function mergeArray(oldArray, newArray, id, newCallback) {
       }
     }
   }
+
   if (numSeen < numOld) {
     // Go through and remove unseen indexes.
     for (i=oldLength; i--;) {

commit 9e61d14dafee5ad139f32d7a83f6a5aa9f2c3ebe

fix(INModelObject): fix regression in INModelObject#update
e0e0151db68dd10cea1431f8742bb89d2c2fe6f5 introduced a regression which caused properties which did
not appear in both the client JSON and raw JSON to be deleted from a resource object, which meant
messageIDs, threadIDs, and so on, were removed.

This is fixed with this CL.

Closes #34

src/core/api/model.js
@@ -166,7 +166,10 @@ INModelObject.prototype.reload = function() {
  * ResourceMapping. Unknown properties are ignored.
  */
 INModelObject.prototype.update = function(data) {
+  if (!data) return;
   var mapping = this.resourceMapping;
+  var updated = data.__converted_from_raw__ || false;
+
   forEach(mapping, function copyMappedProperties(mappingInfo, propertyName) {
     var cast = mappingInfo.to;
     var merge = mappingInfo.merge;
@@ -174,12 +177,14 @@ INModelObject.prototype.update = function(data) {
     var cnst = mappingInfo.cnst;
     var currentValue;
     var isObject;
+    var key = updated ? propertyName : jsonKey;
 
-    if (hasProperty(data, jsonKey)) {
+
+    if (hasProperty(data, key)) {
       if (cnst) {
         this[propertyName] = cnst;
       } else {
-        currentValue = data[jsonKey];
+        currentValue = data[key];
         if (typeof currentValue !== 'undefined') {
           cast = cast(currentValue, mappingInfo);
           isObject = cast && typeof cast === 'object';
@@ -503,6 +508,7 @@ function convertFromRaw(object, resource) {
       object[propertyName] = cnst;
     }
   });
+  defineProperty(object, '__converted_from_raw__', INVISIBLE, null, null, true);
 }
 
 

test/api/threads.spec.js
@@ -276,7 +276,7 @@ describe('INThread', function() {
     });
 
 
-    iit('should update threads consistent with resourceMapping (issue #30)', function() {
+    it('should update threads consistent with resourceMapping (issue #30)', function() {
       var oldThreads = [new INThread(inbox, mockThread1)];
       var fulfilled = jasmine.createSpy('load').andCallFake(function(threads) {
         expect(threads[0]).toContainObject(mappedThread1Updated2);